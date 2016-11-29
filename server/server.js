dc137bc42adda8cd20033c3eefa9803fdd5f00c2
Eduardo Vital <vitaldu@gmail.com>
2016-11-29 11:47:17 -0200

    Using Express and adding post route to Notes
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo(req.body);
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
