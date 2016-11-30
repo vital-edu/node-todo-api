0bd08e2948585e081618538b6d9de172912cbc29
Eduardo Vital <vitaldu@gmail.com>
2016-11-30 18:47:15 -0200

    Adding GET /todos route
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   text: 'Eat lunch'
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    name: 'Eduardo'
+  }, {
+    $set: {
+      name: 'João'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.close();
+});
