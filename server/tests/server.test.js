d737167f8d4dfee84f3f449fdba9d201f53fd6e7
Eduardo Vital <vitaldu@gmail.com>
2016-12-02 15:19:23 -0200

    Adding tests to GET /todos/:id and fixing others
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID,
   text: 'First test todo',
 },{
+  _id: new ObjectID,
   text: 'Second test todo',
 }];
 
@@ -52,13 +55,44 @@ describe('POST /todos', () => {
     });
   });
 
-  it('should get all todos', (done) => {
-    request(app)
-      .get('/todos')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
-      })
-      .end(done());
+  describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /todos/:id', () => {
+    it('should get note that matches id', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+      var newID = new ObjectID;
+
+      request(app)
+        .get(`/todos/${newID}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if todo id is invalid', (done) => {
+      request(app)
+        .get('/todos/invalidID')
+        .expect(404)
+        .end(done);
+    })
+
   });
 });
