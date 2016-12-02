de65930a9ef8542165739727ed567d60bdf7de7e
Eduardo Vital <vitaldu@gmail.com>
2016-12-02 17:47:18 -0200

    Add lodash dependencie and PATCH  /todos/:id route
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -34,7 +35,7 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
@@ -64,6 +65,30 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 })
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
