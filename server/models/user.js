56d4f3a98ccfdfff7de624c1c63e55454e3a6188
Eduardo Vital <vitaldu@gmail.com>
2016-12-03 22:57:52 -0200

    Hash user password before save
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const validator = require('validator');
-const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
@@ -70,6 +71,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth',
   });
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    })
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
