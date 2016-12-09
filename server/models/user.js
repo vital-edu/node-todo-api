a57a14e17f25d80f4b2432c696ed334fbd205b7a
Eduardo Vital <vitaldu@gmail.com>
2016-12-09 16:09:21 -0200

    Add test to POST /users/login and fix another test
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/models/user.js
+++ server/models/user.js
@@ -79,7 +79,6 @@ UserSchema.statics.findByCredentials = function (email, password) {
     if (!user) {
       return Promise.reject();
     }
-    console.log(user);
     return new Promise((resolve, reject) => {
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
